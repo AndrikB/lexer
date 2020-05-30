4f8cbc0ce5ee6059569e57ce248123c2c1949ba4
AndrikB <blagij00@gmail.com>
2020-05-30 17:56:12 +0300

    add style to html

--- lexer/src/main/java/com/blagij/Printer.java
+++ lexer/src/main/java/com/blagij/Printer.java
@@ -12,6 +12,42 @@ public class Printer {
     }
 
     public void printInHTML(String filename) {
+        String style = ".tooltip {\n" +
+                "  position: relative;\n" +
+                "}\n" +
+                "\n" +
+                ".tooltip .tooltiptext {\n" +
+                "  visibility: hidden;\n" +
+                "  width: 120px;\n" +
+                "  background-color: #555;\n" +
+                "  color: #fff;\n" +
+                "  text-align: center;\n" +
+                "  border-radius: 6px;\n" +
+                "  padding: 5px 0;\n" +
+                "  position: absolute;\n" +
+                "  z-index: 1;\n" +
+                "  bottom: 125%;\n" +
+                "  left: 50%;\n" +
+                "  margin-left: -60px;\n" +
+                "  opacity: 0;\n" +
+                "  transition: opacity 0.3s;\n" +
+                "}\n" +
+                "\n" +
+                ".tooltip .tooltiptext::after {\n" +
+                "  content: \"\";\n" +
+                "  position: absolute;\n" +
+                "  top: 100%;\n" +
+                "  left: 50%;\n" +
+                "  margin-left: -8px;\n" +
+                "  border-width: 5px;\n" +
+                "  border-style: solid;\n" +
+                "  border-color: #555 transparent transparent transparent;\n" +
+                "}\n" +
+                "\n" +
+                ".tooltip:hover .tooltiptext {\n" +
+                "  visibility: visible;\n" +
+                "  opacity: 1;\n" +
+                "}";
         StringBuilder html = new StringBuilder(
                 "<html lang=\"en\">\n" +
                         "<head>\n" +
@@ -19,18 +55,24 @@ public class Printer {
                         "\n" +
                         "    <title>Lexer c++ </title>\n" +
                         "\n" +
-                        "<style>body{white-space: pre-wrap;}</style>\n" +
+                        "<style>" +
+                        "body{white-space: pre-wrap;}" +
+                        "span {margin-left: -2px;}" +
+                        style +
+                        "</style>\n" +
                         "</head>\n" +
                         "<body>");
-        final String template = "<font color='#%s'>%s</font>";
+        final String template = "<span class='tooltip'><font color='#%s'>%s</font>" +
+                "  <span class='tooltiptext'>%s</span>" +
+                "</span>";
         for (Token token : tokens) {
-            html.append(String.format(template, token.getTokenType().getColor(), token.getValue()));
+            html.append(String.format(template, token.getTokenType().getColor(), token.getValue(), token.getTokenType()));
         }
         html.append("</body>\n" +
                 "</html>");
 
         try {
-            FileWriter myWriter = new FileWriter(filename+".html");
+            FileWriter myWriter = new FileWriter(filename + ".html");
             myWriter.write(html.toString());
             myWriter.close();
             System.out.println("write correct");
